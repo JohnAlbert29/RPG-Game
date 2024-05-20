c680ace5caf76d952f4ffba5468ec9fc27b381b8
JohnAlbert29 <johnalbert.retiza@tup.edu.ph>
2024-05-20 21:37:20 +0800

    Updating the Rpg game py
    Enhancing the Source to be Readable

--- RPG game.py
+++ RPG game.py
@@ -1,118 +1,110 @@
 import os
 
-os.system('cls')
+os.system('cls' if os.name == 'nt' else 'clear')
 
 class Character:
-    def __int__(self, charClass: str, charWeapon: str, charAbility: str):
-        self.setcharClass = charClass
-        self.setcharWeapon = charWeapon
-        self.setcharAbility = charAbility
+    def __init__(self, char_class: str = "", char_weapon: str = "", char_ability: list = None):
+        self.char_class = char_class
+        self.char_weapon = char_weapon
+        self.char_ability = char_ability if char_ability is not None else []
 
-  
-  
-    def SetClass(self, x):
-        if x == 1:
-            self.setcharClass = "Wizard"
-            return self.setCharClass
-        elif x == 2:
-            self.setcharClass = "Knight"
-            return self.setcharClass
-        elif x == 3:
-            self.setcharClass = "Archer"
-            return self.setcharClass
-        elif x == 4:
-            self.setcharClass = "Assasin"
-            return self.setcharClass
-        else:
+    def set_class(self, x: int) -> str:
+        classes = {
+            1: "Wizard",
+            2: "Knight",
+            3: "Archer",
+            4: "Assassin"
+        }
+        self.char_class = classes.get(x, None)
+        if not self.char_class:
             print("Invalid Input")
+        return self.char_class
 
-    
-    
-    def SetWeapon(self, y):
-        if y == 1:
-            self.setcharWeapon = "Wizard Staff"
-            return self.setcharWeapon
-        elif y == 2:
-            self.setcharWeapon = "Sword"
-            return self.setcharWeapon
-        elif y == 3:
-            self.setcharWeapon = "Bow & Arrow"
-            return self.setcharWeapon
-        elif y == 4:
-            self.setcharWeapon = "Katar"
-            return self.setcharWeapon
-        else:
+    def set_weapon(self, y: int) -> str:
+        weapons = {
+            1: "Wizard Staff",
+            2: "Sword",
+            3: "Bow & Arrow",
+            4: "Katar"
+        }
+        self.char_weapon = weapons.get(y, None)
+        if not self.char_weapon:
             print("Invalid Input")
+        return self.char_weapon
 
-   
-   
-   
-    def SetAbility(self, x, z):
-        if x == 1:
-            self.wizard = ["Energy Ball", "Dragons Breath", "Crowns of Flame", "Hail Storm"]
-            return self.wizard[z]
-        elif x == 2:
-            self.knight = ["Fire Slash", "Power Slash", "Gigantic Storm", "Chaotic Disaster"]
-            return self.knight[z]
-        elif x == 3: 
-            self.archer = ["Take Aim", "Quick Shot", "Blazing Arrow", "Frost Arrow"]
-            return self.archer[z]
-        elif x == 4:
-            self.assassin = ["Cloaking", "Enchant Poison", "Sonic Acceleration", "Meteor Assault"]
-            return self.assassin[z]
-        else:
-            print("Invalid Input")
-
+    def set_ability(self, x: int, z: int) -> str:
+        abilities = {
+            1: ["Energy Ball", "Dragons Breath", "Crowns of Flame", "Hail Storm"],
+            2: ["Fire Slash", "Power Slash", "Gigantic Storm", "Chaotic Disaster"],
+            3: ["Take Aim", "Quick Shot", "Blazing Arrow", "Frost Arrow"],
+            4: ["Cloaking", "Enchant Poison", "Sonic Acceleration", "Meteor Assault"]
+        }
+        ability_list = abilities.get(x, None)
+        if not ability_list:
+            print("Invalid Class Input")
+            return ""
+        try:
+            return ability_list[z]
+        except IndexError:
+            print("Invalid Ability Input")
+            return ""
 
 character = Character()
-print("Class Selection:\n")
-print("""1 Wizard
- 2 Knight
- 3 Archer
- 4 Assassin""")
-x = int(input("class of your Character: "))
-character.SetClass(x)
 
-print("\npick your weapon!")
-print(""" 1 Wizard Staff
- 2 Sword
- 3 Bow & Arrow
- 4 Katar""")
+print("Class Selection:\n")
+print("""1. Wizard
+2. Knight
+3. Archer
+4. Assassin""")
+x = int(input("Choose the class of your Character: "))
+char_class = character.set_class(x)
+if not char_class:
+    exit()
+
+print("\nPick your weapon!")
+print("""1. Wizard Staff
+2. Sword
+3. Bow & Arrow
+4. Katar""")
 y = int(input("Choose the weapon of your Character: "))
-character.SetWeapon(y)
+char_weapon = character.set_weapon(y)
+if not char_weapon:
+    exit()
 
 print("""\nFor WIZARD: 
-    0 Energy Ball
-    1 Dragons Breath
-    2 Crowns of Flame
-    3 Hail Storm
+    0. Energy Ball
+    1. Dragons Breath
+    2. Crowns of Flame
+    3. Hail Storm
 
 For KNIGHT:
-    0 Fish Slash
-    1 Power Slash
-    2 Gigantic Storm
-    3 Chaotic Disaster
+    0. Fire Slash
+    1. Power Slash
+    2. Gigantic Storm
+    3. Chaotic Disaster
 
 For ARCHER:
-    0 Take Aim
-    1 Quick Shot
-    2 Blazing Arrow
-    3 Frost Arrow
+    0. Take Aim
+    1. Quick Shot
+    2. Blazing Arrow
+    3. Frost Arrow
 
 For ASSASSIN:
-    0 Cloaking
-    1 Enchant Poison
-    2 Sonic Acceleration
-    3 Meteor Assault\n""")
+    0. Cloaking
+    1. Enchant Poison
+    2. Sonic Acceleration
+    3. Meteor Assault\n""")
 
-print("Grant "+ character.SetClass(x)+  "'s three abilities: ")
-ability1 = int(input("pick your ability 1: "))
-character.SetAbility(x, ability1)
-ability2 = int(input("pick your ability 2: "))
-character.SetAbility(x, ability2)
-ability3 = int(input("pick your ability 3: "))
-character.SetAbility(x, ability3)
+print(f"Grant {char_class}'s three abilities:")
+abilities = []
+for i in range(3):
+    ability = int(input(f"Pick ability {i+1}: "))
+    ability_name = character.set_ability(x, ability)
+    if ability_name:
+        abilities.append(ability_name)
+    else:
+        exit()
 
-print("\nYou are/an", (character.SetClass(x)))
-print("has a weapon of", (character.SetWeapon(y)))
-print("and has the power of", character.SetAbility(x, ability1) + ",", character.SetAbility(x, ability2)+ ", and", character.SetAbility(x, ability3))
+print(f"\nYou are a(n) {char_class}")
+print(f"With a weapon of {char_weapon}")
+print(f"And have the powers of {', '.join(abilities)}")
